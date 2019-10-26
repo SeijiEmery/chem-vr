3eb32e8fe59479ce72e63ef3d0fbe16bffe2d639
Seiji Emery <SeijiEmery@users.noreply.github.com>
2019-10-26 08:53:06 -0700

    bond bugfixing (one significant bug left)

--- Assets/Scripts/AtomicBond.cs
+++ Assets/Scripts/AtomicBond.cs
@@ -6,26 +6,31 @@ public class AtomicBond : MonoBehaviour
 {
 
     float initialLength = 1f;
+    Atom first = null;
+    Atom second = null;
+
     public void Start () { initialLength = transform.localScale.y; }
     public void SetBond(Atom first, Atom second)
     {
-        var p1 = first.transform.position;
-        var p2 = second.transform.position;
-        transform.position = (p1 + p2) * 0.5f;
-        var dist = Vector3.Distance(p1, p2);
-        //transform.rotation = Quaternion.LookRotation((p1 - p2).normalized, Vector3.Cross(p1, p2).normalized);
-        transform.LookAt(p1);
-        //transform.rotation *= (Quaternion.AngleAxis(90f, Vector3.forward));
-        transform.localScale = new Vector3(
-            transform.localScale.x,
-            transform.localScale.y,
-            initialLength * dist);
-            //transform.localScale.z);
+        this.first = first; this.second = second;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (first != null && second != null)
+        {
+            var p1 = first.transform.position;
+            var p2 = second.transform.position;
+            transform.position = (p1 + p2) * 0.5f;
+            var dist = Vector3.Distance(p1, p2);
+            //transform.rotation = Quaternion.LookRotation((p1 - p2).normalized, Vector3.Cross(p1, p2).normalized);
+            transform.LookAt(p1);
+            //transform.rotation *= (Quaternion.AngleAxis(90f, Vector3.forward));
+            transform.localScale = new Vector3(
+                transform.localScale.x,
+                transform.localScale.y,
+                dist);
+        }
     }
 }
--- Assets/Scripts/Tools/DrawMolecule.cs
+++ Assets/Scripts/Tools/DrawMolecule.cs
@@ -11,6 +11,7 @@ public class DrawMolecule : HandTrackedInputReciever
     FixedJoint connectAtomToHandControllerJoint;
     Atom[] atoms;
     public float bondFormationRadius;
+    public float minBondFormationRadius = 0.01f;
 
     struct Bond { public int from; public int to; }
     List<AtomicBond> newBonds = new List<AtomicBond>();
@@ -20,11 +21,12 @@ public class DrawMolecule : HandTrackedInputReciever
         Debug.Log("trigger pressed: " + info);
         if (drawnAtom == null)
         {
+            atoms = gameObject.GetComponentsInChildren<Atom>();
             drawnAtom = GameObject.Instantiate(atomPrefab.gameObject, info.transform.position, info.transform.rotation, transform);
             drawnAtom.transform.position = info.transform.position;
             connectAtomToHandControllerJoint = drawnAtom.AddComponent<FixedJoint>();
             connectAtomToHandControllerJoint.connectedBody = info.rigidbody;
-            atoms = gameObject.GetComponentsInChildren<Atom>();
+            newBonds.Clear();
         }
     }
     public override void OnTriggerReleased(HandTrackedInfo info)
@@ -49,31 +51,31 @@ public class DrawMolecule : HandTrackedInputReciever
     public void FixedUpdate()
     {
         if (atoms == null || drawnAtom == null) return;
-
         int i = 0; AtomicBond bond;
         foreach (var atom in atoms)
         {
             var dist = Vector3.Distance(atom.transform.position, drawnAtom.transform.position);
-            Debug.Log("" + i + ": " + dist + ": " + (dist <= bondFormationRadius));
-            if (dist <= bondFormationRadius)
+            //Debug.Log("" + i + ": " + dist + ": " + (dist <= bondFormationRadius));
+            if (dist <= bondFormationRadius && dist > minBondFormationRadius)
             {
-                //if (i < atoms.Length)
-                //{
-                //    bond = newBonds[i];
-                //} else
-                //{
+                if (i < newBonds.Count)
+                {
+                    bond = newBonds[i++];
+                } else
+                {
                     bond = GameObject.Instantiate(bondPrefab, transform.position, transform.rotation, transform);
                     newBonds.Add(bond);
-                //}
+                }
                 bond.SetBond(atom, drawnAtom.GetComponent<Atom>());
             }
-            ++i;
         }
+       Debug.Log("found " + i + " bonds this cycle, have " + newBonds.Count);
        while (i < newBonds.Count)
        {
-            var obj = newBonds[i];
+            var j = newBonds.Count - 1;
+            var obj = newBonds[j];
             GameObject.Destroy(obj);
-            newBonds.RemoveAt(i);
-       }
+            newBonds.RemoveAt(j);
+      }
     }
 }
