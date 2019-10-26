6d721257a2162dba82bac0018f48ee6d20527483
Seiji Emery <SeijiEmery@users.noreply.github.com>
2019-10-26 14:19:43 -0700

    start periodic table

---
+++ Assets/Scripts/AtomicTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Material))]
+public class AtomicTemplate : MonoBehaviour
+{
+    public float mass;
+    public float radius;
+    public float name;
+    public float symbol;
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Material>().color = color;
+        transform.localScale = Vector3.one * radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
